2a842252a00dacf165814707fc3ab7c5f5ca57cf
hagoodwi <artemonpas@gmail.com>
2023-11-14 21:38:28 +0300

    Создал модель role

---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,20 @@
+class RolesController < ApplicationController
+    def index
+        @roles = Role.all
+    end
+
+    def new
+        @role = Role.new
+    end
+
+    def create
+        @role = Role.new(role_params)
+        if @role.save
+            flash[:notice] = "Роль создана"
+            redirect_to @course
+        else
+            flash[:notice] = "Произошла ошибка"
+            render 'new'
+        end
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_11_161109) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_14_151147) do
   create_table "courses", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
@@ -34,6 +34,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_11_161109) do
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_groups_users_on_group_id"
     t.index ["user_id"], name: "index_groups_users_on_user_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_profiles", force: :cascade do |t|
