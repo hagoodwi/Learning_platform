9d8fe0cd022a3691f7c3dfed957b87185ee4f1e6
Yann <yann40@yandex.ru>
2023-12-06 03:17:21 +0300

    большой фикс разных багов

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -12,13 +12,14 @@ class MaterialsController < ApplicationController
 
   def create
     @material = @discipline.materials.build(material_params)
+    if @material.name.blank? && params[:material][:file].respond_to?(:original_filename)
+      @material.name = params[:material][:file].original_filename
+    end
     if params[:material][:file].nil?
       redirect_to request.referer, alert: 'Пожалуйста, выберите файл.'
     elsif @material.save
       create_material_access(@material, params[:material])
-      respond_to do |format|
-        format.js
-      end
+      redirect_to request.referer, notice: 'Материал загружен'   
     else
       redirect_to request.referer, alert: 'Ошибка при добавлении материала.'
     end
@@ -34,13 +35,28 @@ class MaterialsController < ApplicationController
     @material = Material.find(params[:id])
     @material_access = @material.material_access || @material.build_material_access
   
-    if @material_access && @material_access.update(material_access_params)
-      respond_to do |format|
-        format.js { render :update_access_success }
+    # Проверим, нужно ли обновлять временные параметры
+    if !@material_access.always_open
+      # Теперь можно обновлять start_time_material_access и end_time_material_access
+      if @material_access.update(material_access_params)
+        respond_to do |format|
+          format.js { render :update_access_success }
+        end
+      else
+        respond_to do |format|
+          format.js { render :update_access_error, status: :unprocessable_entity }
+        end
       end
     else
-      respond_to do |format|
-        format.js { render :update_access_error, status: :unprocessable_entity }
+      # Если always_open, просто обновляем флаг
+      if @material_access.update(material_access_params)
+        respond_to do |format|
+          format.js { render :update_access_success }
+        end
+      else
+        respond_to do |format|
+          format.js { render :update_access_error, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -57,16 +73,29 @@ class MaterialsController < ApplicationController
   end
 
   def create_material_access(material, params)
-    visible_for_a_period = params[:visible_for_a_period] == '1'
+    start_time = params[:start_time]
+    end_time = params[:end_time]
+  
+    if start_time.present? && end_time.present? && start_time.to_datetime >= end_time.to_datetime
+      # Если start_time >= end_time, то выбросим исключение или обработаем ошибку в соответствии с вашими потребностями
+      raise ArgumentError.new('Время начала должно быть раньше времени окончания')
+    end
+  
+    if start_time.present? && end_time.present?
+      always_open = false
+    else
+      always_open = true
+      start_time = end_time = nil
+    end
   
     MaterialAccess.create!(
       material_id: material.id,
-      always_open: !visible_for_a_period,
-      start_time: visible_for_a_period ? params[:start_time] : nil,
-      end_time: visible_for_a_period ? params[:end_time] : nil
+      always_open: always_open,
+      start_time: start_time,
+      end_time: end_time
     )
   end
-
+  
   def material_params
     params.require(:material).permit(:file, :name, :discipline_id)
   end
